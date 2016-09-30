d92021fbc2988efee48994fae768a712bb328fa7
Larry Eisenstein <LEisenstein@carelike.com>
2016-09-30 08:39:30 -0400

    Blank ASP.Net MVC site with WebApi

---
+++ Learn-Auth0-AspNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Learn_Auth0_AspNet.MvcApplication" Language="C#" %>
---
+++ Learn-Auth0-AspNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learn_Auth0_AspNet.Startup))]
+namespace Learn_Auth0_AspNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
